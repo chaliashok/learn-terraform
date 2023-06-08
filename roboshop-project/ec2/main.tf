0f932a93a98ccb14bb4fe9ff3c6b81bc02239e96
chaliashok <chali.ashok@gmail.com>
2023-06-07 23:47:05 -0500

    Terraform learning

--- roboshop-project/ec2/main.tf
+++ roboshop-project/ec2/main.tf
@@ -7,6 +7,22 @@ resource "aws_instance" "web" {
   }
 }
 
+ provisioner "remote-exec" {
+   connection
+   {
+    type        = ssh
+    host        = self.public_ip
+    user        = "centos"
+    password    = "DevOps321"
+  }
+
+  inline = [
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/chaliashok/roboshop-ansible.git main.yml -e env=dev -e role_name=${var.name}"
+   ]
+  }
+
+
 resource "aws_route53_record" "example" {
   zone_id = "Z0783442RLRP3KGA9XLU"
   name    = "${var.name}-dev"  # Replace with your desired subdomain
