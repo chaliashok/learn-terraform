b4b9086962b7ed18b185d611043d981e53c51fe5
chaliashok <chali.ashok@gmail.com>
2023-06-07 23:09:24 -0500

    Terraform learning

--- roboshop-project/ec2/main.tf
+++ roboshop-project/ec2/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"  # Replace with your desired AMI ID
   instance_type = "t3.small"                # Replace with your desired instance type
-  security_groups = ["allow_all"]   # Replace with your security group name
 
   tags = {
     Name = var.name
