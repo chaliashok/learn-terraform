1c461838666fcf1c56f8a75e1d4a7a91576583bc
chaliashok <chali.ashok@gmail.com>
2023-06-07 20:35:27 -0500

    Terraform learning

--- 03-vars/var.tf
+++ 03-vars/var.tf
@@ -17,3 +17,33 @@ output "sample1" {
 output "sample-txt" {
   value = "value of sample - ${var.sample1}"
 }
+
+#Plain variable declaration
+variable "course" {
+  default = "DevOps Training"
+}
+#List variable declaration
+variable "courses" {
+  default = [
+  "devops",
+  "Aws",
+  "python"
+  ]
+}
+
+#map variable declaration
+
+variable "cour_detail" {
+  default = {
+    devops= {
+      name = "Devops"
+      Batch = 73
+      Time = 10
+    }
+    aws = {
+      name = aws
+      batch = 90
+      time = 11
+    }
+  }
+}
