df29d5b0b4114bf7416a5e60cfeee436ae3dc378
chaliashok <chali.ashok@gmail.com>
2023-06-07 20:13:03 -0500

    Terraform learning

--- 02-output/output.tf
+++ 02-output/output.tf
@@ -1,12 +1,15 @@
-output "instance_ips" {
-  value = aws_instance.web.*.public_ip
-}
-
-output "lb_address" {
-  value = aws_alb.web.public_dns
-}
-
-output "password" {
-  sensitive = true
-  value = var.secret_password
+#output "instance_ips" {
+#  value = aws_instance.web.*.public_ip
+#}
+#
+#output "lb_address" {
+#  value = aws_alb.web.public_dns
+#}
+#
+#output "password" {
+#  sensitive = true
+#  value = var.secret_password
+#}
+output "system" {
+  value="hello world"
 }
