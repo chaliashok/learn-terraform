f091e0ac995c07007a5a19d047fe0f1fac8ea31b
chaliashok <chali.ashok@gmail.com>
2023-06-10 19:26:53 -0500

    Terraform learning

--- o4-var1/var.tf
+++ o4-var1/var.tf
@@ -13,3 +13,33 @@ output "sample1" {
 output "sample-exe" {
   value = "value of sample - ${var.sample}"
 }
+#list
+variable "courses" {
+  default = [
+  "Aws",
+  "python",
+  "Devops"
+  ]
+}
+#map
+variable "course_details" {
+  default = {
+    Devops = {
+      name     = "devops"
+      time     = "10:00 AM"
+      duration = "90days"
+    }
+    Aws = {
+      name     = "Aws"
+      time     = "11:00 AM"
+      duration = "90days"
+    }
+  }
+}
+
+output "courses" {
+  value = var.courses
+}
+output "course_details" {
+  value = var.course_details
+}
